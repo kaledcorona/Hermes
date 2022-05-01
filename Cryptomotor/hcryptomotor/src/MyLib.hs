482fc4068f41ffaf53bdeacf1307c5371a9331d1
kaco <imkaco+dev@kaco.dev>
2022-05-01 17:27:39 +0000

    Cambiamos de libreria a Cryptonite

--- Cryptomotor/hcryptomotor/src/MyLib.hs
+++ Cryptomotor/hcryptomotor/src/MyLib.hs
@@ -4,7 +4,6 @@ module MyLib (someFunc) where
 import RIO
 import qualified Data.Text.IO                 as T
 import qualified Data.Text                    as DText
-import qualified Crypto.Cipher.RSA            as RSA
 import qualified Crypto.Curve25519.Pure       as Curve
 import qualified Crypto.Random.AESCtr         as RandomAES
 import qualified Crypto.Random                as Random
@@ -16,7 +15,7 @@ someFunc :: IO ()
 someFunc = do
     g <- Random.newGenIO :: IO AESRNG
     let Right ((pub,priv),g1) = generate g (512 {- number of bits large -}) (7 {- a prime -})
-    msg = "This is my message"
-    Right (ct,g2) = encrypt g1 pub msg
+        msg = "This is my message"
+        Right (ct,g2) = encrypt g1 pub msg
     print $ decrypt priv ct
     print $ decrypt priv ct == Right msg
